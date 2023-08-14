47994a8cea2b722b7d5d1fecd469d767541e7cc9
briaar <briarusss@gmail.com>
2023-08-14 15:48:18 -0400

    completed prompts 5 and 6

--- index17.js
+++ index17.js
@@ -7,24 +7,31 @@ const drinkList = coffeeMenu.map(coffeeMenu =>
 
 console.table(drinkList);
 
-
-
 // 3. Print an array of drinks that cost 5 and under.
 
 const drinkCost = coffeeMenu.filter(coffeeMenu => coffeeMenu.price <= 5);
 
-
 console.table(drinkCost);
 
-
 // 4. Print an array of drinks that are priced at an even number.
+
 const drinkEven = coffeeMenu.filter(coffeeMenu => coffeeMenu.price % 2 === 0);
 
-console.log(drinkEven);
 console.table(drinkEven);
 
 // 5. Print the total if you were to order one of every drink. //
 
+const totalDrinkPrice = coffeeMenu.reduce(coffeeMenu => coffeeMenu.price + total,0);
+
+console.log(totalDrinkPrice)
+
 // 6. Print an array with all the drinks that are seasonal. //
+const seasonalArray = coffeeMenu.filter(coffee => coffee.seasonal === true);
+
+console.log(seasonalArray);
 
 // 7. Print all the seasonal drinks with the words "with imported beans" after the item name. For example: "affogato with imported beans" //
+
+const seasonalDrinksWithWords = coffeeMenu.map(coffeeMenu => coffeeMenu.name+("with imported beans"));
+
+console.table(seasonalDrinksWithWords);
